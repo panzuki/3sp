fdb46ebdfffb3406dcb3aecc554a480155621777
pan <77523808+panzuki@users.noreply.github.com>
2024-10-03 10:00:36 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,40 @@
+fetch('./js/swiper-bundle.min.js').then(r => { return r.text() }).then(t => {
+let tag=['14400','D.C.6000','D.C.4000','d','e'];
+  const mySwiper_sub = new Swiper('.flow01 .swiper-sub', {
+    spaceBetween: 24,
+    grabCursor: true,
+    nested: true,
+    pagination: {
+      el: '.flow01 .swiper-pagination-sub',
+      clickable: true,
+    },
+  });
+
+  const mySwiper_main = new Swiper('.flow01 .swiper-main', {
+    spaceBetween: 24,
+    centeredSlides: true,
+    grabCursor: true,
+    pagination: {
+      el: '.flow01 .swiper-pagination-main',
+      clickable: true,
+      renderBullet: (index, className) => {
+        let num = ('00' + (index + 1)).slice(-2);
+        // return '<span class="' + className + '"><span class="step">STEP.</span>' + num + '</span>';
+        return '<span class="' + className + '"><span class="step"></span>' + tag[index] + '</span>';
+      },
+    },
+    navigation: {
+      nextEl: '.flow01 .swiper-button-next',
+      prevEl: '.flow01 .swiper-button-prev',
+    },
+    breakpoints: {
+      1025: {
+        spaceBetween: 80,
+      }
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+  });
+});
