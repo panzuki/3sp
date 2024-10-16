ab2d2e707c83da78b409d65b63971f7a9c5d2675
pan <77523808+panzuki@users.noreply.github.com>
2024-10-16 21:40:11 +0900

    Update scriptmap.js

--- js/scriptmap.js
+++ js/scriptmap.js
@@ -8,13 +8,13 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 // Define locations and popups for markers
 var locations = [
-    { latlng: [33.5, 44.4], popup: "Fertile Crescent (10,000 BC) - Origin of Wheat Cultivation" },
-    { latlng: [31.0461, 34.8516], popup: "Egypt (5000 BC) - Bread Becomes Staple Food" },
-    { latlng: [37.9838, 23.7275], popup: "Greece (3000 BC) - Wheat Spreads to the Mediterranean" },
-    { latlng: [41.9028, 12.4964], popup: "Rome (1000 BC) - Public Bakeries in the Roman Empire" },
-    { latlng: [48.8566, 2.3522], popup: "France (500 AD) - Wheat Cultivation and Bread Making Flourish" },
-    { latlng: [51.5074, -0.1278], popup: "United Kingdom (1000 AD) - Bread as a Staple for All Classes" },
-    { latlng: [40.7128, -74.0060], popup: "United States (1700 AD) - Wheat Cultivation Expands to the New World" }
+    { latlng: [33.5, 44.4], popup: "Fertile Crescent (10,000 BC) - Origin of Wheat Cultivation" ,nextp: [1,5,6]},
+    { latlng: [31.0461, 34.8516], popup: "Egypt (5000 BC) - Bread Becomes Staple Food" ,nextp: [2] },
+    { latlng: [37.9838, 23.7275], popup: "Greece (3000 BC) - Wheat Spreads to the Mediterranean" ,nextp: [3] },
+    { latlng: [41.9028, 12.4964], popup: "Rome (1000 BC) - Public Bakeries in the Roman Empire" ,nextp: [4]},
+    { latlng: [48.8566, 2.3522], popup: "France (500 AD) - Wheat Cultivation and Bread Making Flourish",nextp: [none] },
+    { latlng: [51.5074, -0.1278], popup: "United Kingdom (1000 AD) - Bread as a Staple for All Classes" ,nextp: [none] },
+    { latlng: [40.7128, -74.0060], popup: "United States (1700 AD) - Wheat Cultivation Expands to the New World" ,nextp: [none] }
 ];
 
 // Add markers to the map
@@ -78,9 +78,17 @@ slider.addEventListener('input', function() {
 // Function to draw multiple arrows from a single point
 function drawMultipleArrowsFromMarker(markerIndex) {
     var start = locations[markerIndex].latlng;
+    var j = 0;
     // Draw arrows from the selected marker to multiple locations
     for (var i = markerIndex + 1; i < locations.length; i++) {
-        drawArrow(start, locations[i].latlng);
+        for (var j = j + 1; j < locations[i].nextp.length; j++) {
+            if (locations[i].nextp[j] == 'none') {
+                
+            }else{
+                var next = locations[i].nextp[j];
+                drawArrow(start, locations[next].latlng);
+            }
+        }
     }
 }
 
