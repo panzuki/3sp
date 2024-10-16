6fe4d32fd99e369df000252f791d67d8642a0d38
pan <77523808+panzuki@users.noreply.github.com>
2024-10-16 20:41:30 +0900

    Update scriptmap.js

--- js/scriptmap.js
+++ js/scriptmap.js
@@ -30,13 +30,13 @@ locations.forEach(function(location) {
     });
 });
 
-// Function to draw an arrow (polyline) between markers
-var polyline;
+// Array to keep track of all drawn arrows (polylines)
+var polylines = [];
+
+// Function to draw an arrow (polyline) between markers and save it to the array
 function drawArrow(start, end) {
-    if (polyline) {
-        map.removeLayer(polyline);  // Remove previous arrow
-    }
-    polyline = L.polyline([start, end], {color: 'orange'}).addTo(map);
+    var polyline = L.polyline([start, end], {color: 'orange'}).addTo(map);
+    polylines.push(polyline);
 }
 
 // Current marker index to track user's position
@@ -44,21 +44,21 @@ var currentIndex = 0;
 
 // Function to move to the specific marker and draw an arrow
 function moveToMarker(index) {
-    if (index >= 0 && index < locations.length - 1) {
-        drawArrow(locations[index].latlng, locations[index + 1].latlng);
-        markers[index + 1].openPopup();
-        currentIndex = index;
+    if (index > currentIndex) {
+        // Draw arrows for all points between currentIndex and the new index
+        for (var i = currentIndex; i < index; i++) {
+            drawArrow(locations[i].latlng, locations[i + 1].latlng);
+            markers[i + 1].openPopup();
+        }
+    } else if (index < currentIndex) {
+        // Remove arrows when moving backwards (but keep arrows up to the new index)
+        for (var i = polylines.length - 1; i >= index; i--) {
+            map.removeLayer(polylines[i]);
+            polylines.pop();
+        }
     }
+    currentIndex = index;
 }
-
-// Listen for arrow key presses
-document.addEventListener('keydown', function(event) {
-    if (event.key === 'ArrowRight') {
-        moveToMarker(currentIndex + 1);  // Move to the next marker on right arrow
-    } else if (event.key === 'ArrowLeft') {
-        moveToMarker(currentIndex - 1);  // Move to the previous marker on left arrow
-    }
-});
 
 // Add slider control
 var slider = document.getElementById('slider');
@@ -66,6 +66,14 @@ slider.addEventListener('input', function() {
     var index = parseInt(slider.value);
     moveToMarker(index);
 });
+
+// Function to draw multiple arrows from a single point (for the last point)
+function drawMultipleArrowsFromMarker(markerIndex) {
+    var start = locations[markerIndex].latlng;
+    for (var i = markerIndex + 1; i < locations.length; i++) {
+        drawArrow(start, locations[i].latlng);  // Draw arrows to all subsequent locations
+    }
+}
 
 // Start by drawing the first arrow
 drawArrow(locations[0].latlng, locations[1].latlng);
